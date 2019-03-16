1cd03c9b68168d5f4b3ce93cbe593c8c3e81b91a
tryosaurus <tryosaurus@gmail.com>
2019-03-17 04:21:00 +1100

    Crop Functionality

--- front_end/spot/src/Pages/Crop.js
+++ front_end/spot/src/Pages/Crop.js
@@ -1,15 +1,39 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
 import { Jumbotron, Button, Container } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../App.css";
+//import { Slider } from "material-ui-slider";
+import Cropper from "react-easy-crop";
+import ReactCrop from "react-image-crop";
+import getCroppedImg from "../Components/cropImage.js";
 class Crop extends Component {
   constructor(props) {
     super(props);
     this.state = {
       imgSrc: props.location.state.detail,
-      imgForm: props.location.state.imgForm
+      imgForm: props.location.state.imgForm,
+      crop: { x: 3, y: 2 },
+      zoom: 1,
+      aspect: 3 / 3,
+      croppedAreaPixels: null,
+      croppedImage: null
     };
   }
+
+  onCropChange = crop => {
+    this.setState({ crop });
+  };
+
+  onCropComplete = (croppedArea, croppedAreaPixels) => {
+    console.log(croppedArea, croppedAreaPixels);
+    this.setState({ croppedAreaPixels });
+  };
+
+  onZoomChange = zoom => {
+    this.setState({ zoom });
+  };
+
   dataURItoBlob = dataURI => {
     // convert base64/URLEncoded data component to raw binary data held in a string
     var byteString;
@@ -32,9 +56,9 @@ class Crop extends Component {
     return new Blob([ia], { type: mimeString });
   };
 
-  send = () => {
+  send = newImg => {
     const fd = new FormData();
-    var data = this.dataURItoBlob(this.state.imgSrc);
+    var data = this.dataURItoBlob(newImg);
     fd.append("image", data);
     fetch(
       "https://10.77.2.189:5000/predict",
@@ -55,27 +79,58 @@ class Crop extends Component {
           search: "query=abc",
           state: {
             detail: body.confidences,
-            imageData: this.state.imgSrc
+            imageData: newImg
           }
         });
       });
     });
   };
+  sendCroppedImage = async () => {
+    const croppedImage = await getCroppedImg(
+      this.state.imgSrc,
+      this.state.croppedAreaPixels
+    ).then(response => {
+      console.log(response);
+      this.send(response);
+    });
+    this.setState({ croppedImage });
+  };
   render() {
     //var image = JSON.parse(localStorage.getItem("Image"));
     //this.state.imgSrc = image;
     return (
-      <div>
-        <img
+      <div class="d-flex justify-content-center align-items-center column">
+        {this.state.imgSrc && (
+          <div
+            className="crop-container row"
+            style={{
+              backgroundColor: "white",
+              width: "300px",
+              height: "300px"
+            }}
+          >
+            <Cropper
+              showGrid={false}
+              image={this.state.imgSrc}
+              class={"rotateimg90"}
+              crop={this.state.crop}
+              zoom={this.state.zoom}
+              aspect={this.state.aspect}
+              onCropChange={this.onCropChange}
+              onCropComplete={this.onCropComplete}
+              onZoomChange={this.onZoomChange}
+            />
+          </div>
+        )}
+        <Button
+          onClick={this.sendCroppedImage}
           style={{
-            width: "300px",
-            height: "300px",
-            marginTop: "20px",
-            transform: "rotate(90deg)"
+            backgroundColor: "red",
+            position: "absolute",
+            bottom: "20px"
           }}
-          src={this.state.imgSrc}
-        />
-        <Button onClick={this.send} style={{ backgroundColor: "red" }}>
+          class={"row"}
+        >
           Send
         </Button>
       </div>
