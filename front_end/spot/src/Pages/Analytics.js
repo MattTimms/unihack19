4c31ed281a7ed015461baeccc012d79cc8d25438
tryosaurus <tryosaurus@gmail.com>
2019-03-16 23:28:25 +1100

    Initiate

--- front_end/spot/src/Pages/Analytics.js
+++ front_end/spot/src/Pages/Analytics.js
@@ -1,29 +1,27 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import AnalyticsCard from "../Components/AnalyticsCard"
-import analyticsData from "../Data/analyticsData"
+import AnalyticsCard from "../Components/AnalyticsCard";
+import analyticsData from "../Data/analyticsData";
 
 class Analytics extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       data: analyticsData
-    }
+    };
   }
   render() {
-
-    var todo = this.state.data.map(item => <AnalyticsCard id={item.id}
-                                                      status={item.status}
-                                                      location={item.location}
-                                                      date={item.date}/>)
-    console.log(todo)
-    return (
-      <div>
-        {todo}
-      </div>
-    )
+    var todo = this.state.data.map(item => (
+      <AnalyticsCard
+        id={item.id}
+        status={item.status}
+        location={item.location}
+        date={item.date}
+      />
+    ));
+    console.log(todo);
+    return <div>{todo}</div>;
   }
-
 }
 
 export default Analytics;
--- front_end/spot/src/Pages/NewEntry.js
+++ front_end/spot/src/Pages/NewEntry.js
@@ -1,41 +1,42 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import SaveSelectCard from "../Components/SaveSelectCard"
-import analyticsData from "../Data/analyticsData"
-import { Button } from 'reactstrap';
+import SaveSelectCard from "../Components/SaveSelectCard";
+import analyticsData from "../Data/analyticsData";
+import { Button } from "reactstrap";
 
-import { TextInput, Select } from 'grommet';
+import { TextInput, Select } from "grommet";
 
 class Analytics extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       data: analyticsData,
       location: "",
       month: "",
       day: ""
-    }
-    this.handleSave = this.handleSave.bind(this)
-    this.handleChange = this.handleChange.bind(this)
+    };
+    this.handleSave = this.handleSave.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleSave(event){
-    localStorage.setItem("")
-
+  handleSave(event) {
+    localStorage.setItem("");
   }
 
-  handleChange(event){
-    const {name, value} = event.target
-    this.setState({ [name]: value })
-
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
   }
 
   render() {
-
-    var todo = this.state.data.map(item => <SaveSelectCard id={item.id}
-                                                      status={item.status}
-                                                      location={item.location}
-                                                      date={item.date}/>)
+    var todo = this.state.data.map(item => (
+      <SaveSelectCard
+        id={item.id}
+        status={item.status}
+        location={item.location}
+        date={item.date}
+      />
+    ));
     //console.log(todo)
     return (
       <div>
@@ -49,9 +50,8 @@ class Analytics extends Component {
 
         <Button>Save</Button>
       </div>
-    )
+    );
   }
-
 }
 
 export default Analytics;
