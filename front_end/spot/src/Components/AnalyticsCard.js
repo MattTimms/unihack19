e0cbe4011fe9ccdbc6339ac40f2595a3ae141dd4
Johnny Liaw <jxlia3@student.monash.edu>
2019-03-16 17:05:28 +1100

    got outputting list of moles
    list of moles

--- front_end/spot/src/Components/AnalyticsCard.js
+++ front_end/spot/src/Components/AnalyticsCard.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { Route, Link, BrowserRouter as Router, Switch } from "react-router-dom";
 import { Button } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol } from 'mdbreact';
 
 class AnalyticsCard extends Component {
   constructor(props){
@@ -18,19 +17,9 @@ class AnalyticsCard extends Component {
   render() {
     console.log("This is the location:", this.state.location)
     return (
-      <MDBCol>
-      <MDBCard style={{ width: "22rem" }}>
-        <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" waves />
-        <MDBCardBody>
-          <MDBCardTitle>Card title</MDBCardTitle>
-          <MDBCardText>
-            Some quick example text to build on the card title and make
-            up the bulk of the card&apos;s content.
-          </MDBCardText>
-          <MDBBtn href="#">MDBBtn</MDBBtn>
-        </MDBCardBody>
-      </MDBCard>
-    </MDBCol>
+      <div>
+        <p>{this.state.location}</p>
+      </div>
     );
   }
 }
--- front_end/spot/src/Pages/Analytics.js
+++ front_end/spot/src/Pages/Analytics.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import AnalyticsCard from "../Components/AnalyticsCard"
+import analyticsData from "../Data/analyticsData"
+
 class Analytics extends Component {
-  state = {};
+  constructor() {
+    super()
+    this.state = {
+      data: analyticsData
+    }
+  }
   render() {
-    return this.renderPage();
+    var todo = this.state.data.map(item => <AnalyticsCard id={item.id}
+                                                      status={item.status}
+                                                      location={item.location}
+                                                      date={this.dateOfLastPhoto}/>)
+    return (
+      <div>
+        {todo}
+      </div>
+    )
   }
 
-  renderPage = () => {
-    return <div />;
-  };
 }
 
 export default Analytics;
